4bf986ac79125580a634183f51da5ff0e572bbf9
Mauricio Latorre <latorre.mauricio.1992@gmail.com>
2023-01-04 23:01:17 -0300

    Se crea funcionalidad de cuestionario

--- src/app/components/dashboard/cuestionarios/nuevo-cuestionario/paso-dos/nueva-pregunta/nueva-pregunta.component.ts
+++ src/app/components/dashboard/cuestionarios/nuevo-cuestionario/paso-dos/nueva-pregunta/nueva-pregunta.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { Pregunta } from 'src/app/models/pregunta';
+import {Respuesta} from 'src/app/models/respuesta';
 
 @Component({
   selector: 'app-nueva-pregunta',
@@ -12,6 +13,7 @@ export class NuevaPreguntaComponent implements OnInit {
   nuevaPregunta: FormGroup;
   pregunta!: Pregunta;
   rtaCorrecta = 0;
+  @Output() enviarPregunta = new EventEmitter<Pregunta>();
 
   constructor(private fb: FormBuilder, private toastr: ToastrService) { 
     this.nuevaPregunta = this.fb.group({
@@ -21,6 +23,7 @@ export class NuevaPreguntaComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.agregarRespuestasPorDefecto();
   }
 
   get getRespuestas(): FormArray{
@@ -32,5 +35,56 @@ export class NuevaPreguntaComponent implements OnInit {
       descripcion:['', Validators.required],
       esCorrecta: 0
     }))
+  }
+
+  agregarRespuestasPorDefecto(): void{
+    this.agregarRespuestas();
+    this.agregarRespuestas();
+  }
+
+  eliminarRespuesta(index:number):void{
+    if(this.getRespuestas.length ===2){
+      this.toastr.error('La pregunta debe contener al menos dos respuestas', 'Error!');
+    }else{
+      this.getRespuestas.removeAt(index);
+    }
+  }
+
+  setRespuestaValida(index: number):void{
+    this.rtaCorrecta = index;
+  }
+
+  agregarPregunta(): void{
+    //Obtenemos el titulo de la pregunta
+    const descripcionPregunta = this.nuevaPregunta.get('titulo')?.value;
+
+    //Obtenemos el artray de respuestas
+    const arrRespuestas =this.nuevaPregunta.get('respuestas')!.value;
+
+    //Creamos un array de respuestas
+    const arrayRta: Respuesta[] = [];
+
+    arrRespuestas.forEach((element: any, index: any ) => {
+      
+      const respuesta: Respuesta = new Respuesta(element.descripcion, false);
+
+      if (index === this.rtaCorrecta){
+        respuesta.esCorrecta = true;
+      }
+
+      arrayRta.push(respuesta);
+    });
+
+    const pregunta: Pregunta = new Pregunta(descripcionPregunta, arrayRta);
+
+    this.enviarPregunta.emit(pregunta);
+    console.log(pregunta);
+    this.reset();
+  }
+
+  reset():void{
+    this.nuevaPregunta.reset();
+    this.getRespuestas.clear();
+    this.agregarRespuestasPorDefecto();
   }
 }
--- src/app/components/dashboard/cuestionarios/nuevo-cuestionario/paso-dos/paso-dos.component.ts
+++ src/app/components/dashboard/cuestionarios/nuevo-cuestionario/paso-dos/paso-dos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { Cuestionario } from 'src/app/models/cuestionario';
 import { Pregunta } from 'src/app/models/pregunta';
 import { CuestionarioService } from 'src/app/services/cuestionario.service';
 
@@ -10,17 +11,45 @@ import { CuestionarioService } from 'src/app/services/cuestionario.service';
   styleUrls: ['./paso-dos.component.css']
 })
 export class PasoDosComponent implements OnInit {
-  tituloCuestionario: string;
-  descripcionCuestionario: string;
+  tituloCuestionario!: string;
+  descripcionCuestionario!: string;
   listPreguntas: Pregunta[] = [];
+  loading = false;
 
   constructor(private cuestionarioService: CuestionarioService, 
     private toastr: ToastrService, private router: Router) { 
-    this.tituloCuestionario = cuestionarioService.tituloCuestionario;
-    this.descripcionCuestionario = cuestionarioService.descripcionCuestionario;
   }
 
   ngOnInit(): void {
+    this.tituloCuestionario = this.cuestionarioService.tituloCuestionario;
+    this.descripcionCuestionario = this.cuestionarioService.descripcionCuestionario;
   }
 
+  guardarPregunta(pregunta: Pregunta): void{
+    this.listPreguntas.push(pregunta);
+    console.log(this.listPreguntas);
+  }
+
+  eliminarPregunta(index:number):void{
+    this.listPreguntas.splice(index, 1);
+  }
+
+  guardarCuestionario(): void{
+    const cuestionario: Cuestionario = {
+      nombre: this.tituloCuestionario,
+      descripcion: this.descripcionCuestionario,
+      listPregunta: this.listPreguntas
+    }
+    this.loading = true;
+    //Enviamos cuestionario al backend
+    this.cuestionarioService.guardarCuestionario(cuestionario).subscribe(data =>{
+      this.toastr.success('Cuestionario guardado exitosamente','Cuestionario registrado!');
+      this.router.navigate(['/dashboard']);
+      this.loading = false;
+    }, error =>{
+      this.toastr.error('Se ha producido un error y no se guardó el cuestionario','Error!');
+      this.router.navigate(['/dashboard']);
+      this.loading = false;
+    });
+  }
 }
